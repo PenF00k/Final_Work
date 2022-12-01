ea13833bae58d7689636377af3980f9cc557ffee
BatmanovSergeydotnet <xoolly@mail.ru>
2022-12-02 01:30:23 +0300

    Add Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+string[] NewArray(string [] arr)
+{
+    string[] newArr = new string[2];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
+}
+
+string[] array = { "hello, 123, 45, hello2" };
+string[] newArray = NewArray(array);
+PrintArray(newArray);
