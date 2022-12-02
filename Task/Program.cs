1094f350079af4c21a5d4651a5d04ff3dd8d1e37
BatmanovSergeydotnet <xoolly@mail.ru>
2022-12-03 01:30:04 +0300

    Add block.drawio

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,36 +1,64 @@
 ﻿
 void PrintArray(string[] arr) // Метод печати
 {
-    Console.Write("[");
+    Console.Write("[ ");
     for (int i = 0; i < arr.Length; i++)
     {
         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
-    Console.Write("]");
+    Console.Write(" ]");
 }
 
-string[] NewArray(string [] arr) // Метод поиска элементов длиной менее 3-х символов
+// string[] NewArray(string[] arr) // Метод поиска элементов длиной менее 3-х символов
+// {
+//     int k = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i].Length <= 3) k++;
+//     }
+//     string[] newArr = new string[k];
+//     int j = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i].Length <= 3)
+//         {
+//             newArr[j] = arr[i];
+//             j++;
+//         }
+//     }
+//     return newArr;
+// }
+
+// string[] array = { "hello", "123", "456", "hello2", "dft", "der" };
+// string[] newArray = NewArray(array);
+// PrintArray(array);
+// Console.Write("-->");
+// PrintArray(newArray);
+
+string[] NewArray(string[] arr) // Метод поиска элементов длиной менее 3-х символов
 {
+    string[] newArr = new string[arr.Length];
     int k = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i].Length <= 3) k++;
-    }
-    string[] newArr = new string[k];
-    int j = 0;
     for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i].Length <= 3)
         {
-            newArr[j] = arr[i];
-            j++;
+            newArr[k] = arr[i];
+            k++;
         }
     }
-    return newArr;
+    string[] newArr3 = new string[k];
+  
+    for (int i = 0; i < k; i++)
+    {
+        newArr3[i] = newArr[i];
+    }
+    return newArr3;
 }
 
-string[] array = { "hello", "123", "456", "hello2", "dft" };
+string[] array = { "Hi", "hello", "123", "sup", "456", "hello2", "dft", "der", "sup2" };
 string[] newArray = NewArray(array);
 PrintArray(array);
-Console.Write("-->");
+Console.Write(" --> ");
+PrintArray(newArray);
